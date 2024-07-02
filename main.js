5ba67cfbec122d4c99bf5ec9dbcd1f022f2f76dd
Dron <115934404+DronSyperlol@users.noreply.github.com>
2024-07-03 02:13:28 +0500

    Initial

---
+++ main.js
@@ -0,0 +1,59 @@
+const axios = require('axios');
+const https = require('https');
+const fs = require('fs');
+
+const host = '0.0.0.0';
+const port = 3129;
+
+const server = https
+    .createServer(
+        {
+            key: fs.readFileSync('./cert/key.pem'),
+            cert: fs.readFileSync('./cert/cert.pem'),
+        },
+        connectionHandler
+    )
+    .listen(port, host, function () {
+        console.log(
+            `Server listens https://${host}:${port}`
+        );
+    });
+
+
+function connectionHandler(request, result) {
+    console.log("Request: " + request);
+    var requestData = '';
+    request.on('data', (chunk) => {
+        requestData += chunk;
+    });
+    request.on('end', () => {
+        routing(requestData).then((responseData) => {
+            result.end(responseData);
+        }).catch((error) => {
+            result.statusCode = 500;
+            result.end();
+        });
+    })
+}
+
+async function routing(message) {
+    try {
+        var msg = JSON.parse(message);
+    }
+    catch {
+        return 'Bad data';
+    }
+    if (!'url' in msg) return 'Missing required field (url)';
+    if (!'headers' in msg) return 'Missing required field (headers)';
+    if (!msg.url.includes('https://') && !msg.url.includes('http://')) msg.url = "http://" + msg.url;
+    try {
+        var response = await axios.get(msg.url, {
+            headers: msg.headers
+        });
+    }
+    catch (ex) {
+        console.error(ex);
+        return 'Execute error';
+    }
+    return response.data;
+}
